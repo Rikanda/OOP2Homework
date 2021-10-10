7fdf6d17e1ade52a58b44e46dab062313bf5c93d
Natalya <soltk7@yandex.ru>
2021-10-10 12:05:04 +0300

    Домашнее задание ко второму уроку.

---
+++ OOP2Homework/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2Homework
+{
+    class BankAccount
+    {
+
+        int numberAccount;
+        double balanceAccount;
+        Type typeAccount;
+
+        public void InsertNumberAccount(int value)
+        {
+            numberAccount = value;
+        }
+
+        public void InsertBalanceAccount(double value)
+        {
+            balanceAccount = value;
+        }
+
+        public void InsertTypeAccount (Type value)
+        {
+            typeAccount = value;
+        }
+
+        public void PrintAccountData ()
+        {
+            Console.WriteLine("Номер счета: " + numberAccount + "; Баланс счета: " 
+                + balanceAccount + "; Тип счета: " + typeAccount);
+        }
+
+    }
+}
--- OOP2Homework/Program.cs
+++ OOP2Homework/Program.cs
@@ -2,11 +2,25 @@
 
 namespace OOP2Homework
 {
+    public enum Type
+    {
+        Дебетовый,
+        Кредитовый,
+        Накопительный
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount testAccount = new BankAccount();
+            int number = 1234;
+            double balance = 100250.33;
+            Type type = Type.Дебетовый;
+            testAccount.InsertNumberAccount(number);
+            testAccount.InsertBalanceAccount(balance);
+            testAccount.InsertTypeAccount(type);
+            testAccount.PrintAccountData();
+            
         }
     }
 }
