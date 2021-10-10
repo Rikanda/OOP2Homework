5a795eb0ba719eabaa3aca949a90832413cf8273
Natalya <soltk7@yandex.ru>
2021-10-10 11:16:49 +0300

    Add project files.

---
+++ OOP2Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP2Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
